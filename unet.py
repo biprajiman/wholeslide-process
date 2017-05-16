70d244cd28ad8c2ed684c56a94a977c9ec4e1a6d
biprajiman <manish.sapkota@gmail.com>
2017-05-16 11:29:48 -0400

    Update copyright notice.

--- unet.py
+++ unet.py
@@ -1,35 +1,9 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2017 BICI2 Lab University of Florida
+# Author: Manish Sapkota
+# Some of the codes have been adopted from original Tensorflow examples
 # ==============================================================================
 
-"""Builds the CIFAR-10 network.
-
-Summary of available functions:
-
- # Compute input images and labels for training. If you would like to run
- # evaluations, use inputs() instead.
- inputs, labels = distorted_inputs()
-
- # Compute inference on the model inputs to make a prediction.
- predictions = inference(inputs)
-
- # Compute the total loss of the prediction with respect to the labels.
- loss = loss(predictions, labels)
-
- # Create a graph to run one step of training with respect to the loss.
- train_op = train(loss, global_step)
-"""
+"""Builds the UNET network."""
 # pylint: disable=missing-docstring
 # pylint: disable=line-too-long
 
