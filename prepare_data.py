5a780e81962431cd8cef7ade838154293640d375
biprajiman <manish.sapkota@gmail.com>
2017-06-01 12:04:04 -0400

    debugging done

---
+++ prepare_data.py
@@ -0,0 +1,17 @@
+# Read whole slides
+# Read the json data for regions of interest from annotation
+# crop the training images from using the json region annotation data
+
+def get_number_of_annotation_files(path):
+
+    return 
+
+
+def get_regions_from_json(path, ext='.json'):
+    """Get the list for randomly selected rectangular regions of interest"""
+    return 
+
+def save_image_patches(path):
+    """ Randomly sample 75% whole slide images for training and 25% for validation"""
+
+    return
